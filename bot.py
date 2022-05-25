118f4380181c75e644286f11dab69595a0bb831a
jannes.t13@gmail.com <jannes.t13@gmail.com>
2022-05-25 17:18:12 +0200

    Init and add random chat function

---
+++ bot.py
@@ -0,0 +1,92 @@
+import discord
+from discord.ext import commands
+from discord import utils
+from config import TOKEN, PREFIX
+import datetime
+
+COGS = [
+
+]
+
+class Bot(commands.AutoShardedBot):
+    def __init__(self, **kwargs):
+        intents = discord.Intents(
+            guilds=True,
+            members=True,
+            bans=True,
+            emojis=True,
+            voice_states=True,
+            messages=True,
+            reactions=True,
+            presences=True,
+            invites=True
+        )
+        super(Bot, self).__init__(
+            command_prefix=PREFIX,
+            description="Chat with someone random!",
+            intents=intents,
+        )
+        self.launch = __import__("datetime").datetime.utcnow()
+        self.version = "0.0.1"
+        self.creator = "Bambus#8446"
+
+        for ext in COGS:
+            try:
+                self.load_extension(ext)
+            except Exception as e:
+                print(f"Cant load {ext}")
+                raise e
+    
+    async def on_ready(self):
+        await self.change_presence(activity=discord.Game(name=f"{PREFIX}help"))
+        print(f"{self.user.id}\n"f"{utils.oauth_url(self.user.id)}\n"f"{self.user.name}\n""Ready!")
+
+    #async def on_message(self, message):
+    #    """IGNORE PRIVATE MESSAGE"""
+    #    if not message.guild:
+    #        return
+    #    await self.process_commands(message)
+
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.BotMissingPermissions):
+            return await ctx.send(embed=ErrorEmbed(str(error)))
+
+        elif isinstance(error, commands.BotMissingPermissions):
+            return await ctx.send(embed=ErrorEmbed(str(error)))
+        
+        elif isinstance(error, commands.BotMissingRole):
+            return await ctx.send(embed=ErrorEmbed(str(error)))
+        
+        elif isinstance(error, commands.CheckFailure):
+            return await ctx.send(embed=ErrorEmbed(str(error)))
+        
+        elif isinstance(error, commands.CommandNotFound):
+            return await ctx.send(embed=ErrorEmbed("This isn't a command! Please use the `help` command"))
+        
+        elif isinstance(error, commands.BadArgument):
+            owner = self.get_user(self.owner_id)
+            return await owner.send(embed=OwnerErrorEmbed(str(error), ctx.guild.name))
+
+class ErrorEmbed(discord.Embed):
+    def __init__(self, description):
+        super().__init__(
+            title="Error",
+            description=description,
+            color=discord.Color.red(),
+            timestamp=datetime.datetime.utcnow(),
+        )
+        self.set_footer(text=f'{bot.user.name} made with <3 by Bambus#8446', icon_url=bot.user.avatar_url)
+
+class OwnerErrorEmbed(discord.Embed):
+    def __init__(self, description, server):
+        super().__init__(
+            title=f"Error on server {server}",
+            description=f"```{description}```",
+            color=discord.Color.red(),
+            timestamp=datetime.datetime.utcnow(),
+        )
+        self.set_footer(text=f'{bot.user.name} made with <3 by Bambus#8446', icon_url=bot.user.avatar_url)
+
+bot = Bot()
+
+bot.run(TOKEN)
---
+++ cogs/chat.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import random
+
+class Chat(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.queue = []
+
+    @commands.group(name='chat', aliases=["wannachat"], invoke_withou_command=True)
+    async def chat(self, ctx):
+        """SEARCH FOR CHATPARTNERS OR ADD YOU TO THE QUEUE"""
+        if len(self.queue) == 0:
+            self.queue.append(ctx.author.id)
+            return await ctx.reply("There is no other user searching, I put you in the waiting queue! I'll send you a message when someone wanna chat", delete_after=5)
+        
+        if ctx.author.id in self.queue:
+            return await ctx.reply("You are in the waiting queue!", delete_after=4)
+        
+        user = random.choice(self.queue)
+        while user == ctx.author.id:
+            user = random.choice(self.queue)
+        
+        user = self.bot.get_user(user)
+
+        # REMOVE AUTHOR AND CHAT PARTNER FROM QUEUE
+        self.queue.remove(user.id)
+        self.queue.remove(ctx.author.id)
+
+        return await ctx.author.send(embed=discord.Embed(title="I find someone to chat with:", description=f"Add: `{user.name}#{user.discriminator}`").set_thumbnail(url=user.avatar_url))
+
+    @chat.command(name="remove")
+    async def chat_remove(self, ctx):
+        """REMOVES USER FROM QUEUE"""
+        self.queue.remove(ctx.author.id)
+
+        return await ctx.reply("Successfully removed you from the waiting queue!", delete_after=4)
+
+def setup(bot):
+    bot.add_cog(Chat(bot))
