cdd18d7bdc0c1ceafa3223fc330e4953943bd30e
jannes.t13@gmail.com <jannes.t13@gmail.com>
2022-06-14 18:46:04 +0200

    v0.2

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ class Bot(commands.AutoShardedBot):
             intents=intents,
         )
         self.launch = __import__("datetime").datetime.utcnow()
-        self.version = "v0.1.1"
+        self.version = "v0.2"
         self.creator = "Bambus#8446"
         self.ownerid = OWNERID
         self.db = DbClient().collection
