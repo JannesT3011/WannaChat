b886ade5be459e54afa7a7cbcd3d4d50375eb3e7
jannes.t13@gmail.com <jannes.t13@gmail.com>
2022-06-14 15:01:58 +0200

    Add logoff confirm buttons

--- cogs/login.py
+++ cogs/login.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from database.database import Database
 from config import EMBED_COLOR
+from discord.ui import Button, View
 
 class Login(commands.Cog):
     def __init__(self, bot):
@@ -18,12 +19,29 @@ class Login(commands.Cog):
     
     @commands.command(name="logoff")
     async def logoff(self, ctx):
-        try:
-            await Database().delete_db(str(ctx.author.id))
-        except:
-            return await ctx.send("Already logoff!", delete_after=4)
-        await self.bot.queuedb.update_many({"_id": "queue"}, {"$pull": {"queue": str(ctx.author.id)}})
-        return await ctx.author.send(embed=discord.Embed(title="Logoff successful!", color=EMBED_COLOR))
+
+        async def logoff_confirm_button(interaction):
+            try:
+                await Database().delete_db(str(ctx.author.id))
+            except:
+                await msg.delete()
+                return await ctx.author.send("Already logoff!")
+            await self.bot.queuedb.update_many({"_id": "queue"}, {"$pull": {"queue": str(ctx.author.id)}})
+
+            await msg.delete()
+            return await ctx.author.send(embed=discord.Embed(title="Logoff successful!", color=EMBED_COLOR))
+
+        async def logoff_cancel_button(interaction):
+            return await msg.delete()
+
+        confirm_button = Button(label="Yes", style=discord.ButtonStyle.red, emoji="✅")
+        cancel_button = Button(label="Cancel", style=discord.ButtonStyle.green, emoji="❌")
+        confirm_button.callback = logoff_confirm_button
+        cancel_button.callback = logoff_cancel_button
+        view = View()
+        view.add_item(confirm_button)
+        view.add_item(cancel_button)
+        msg = await ctx.author.send(embed=discord.Embed(title="Are you sure you want to delete your account?", color=EMBED_COLOR), view=view)
 
 async def setup(bot):
     await bot.add_cog(Login(bot))
