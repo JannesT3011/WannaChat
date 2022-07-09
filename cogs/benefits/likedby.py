7bf3ffe6e817f60f504d6d7e2912b615a8a1d8a6
jannes.t13@gmail.com <jannes.t13@gmail.com>
2022-07-09 19:56:01 +0200

    Init likedby command

---
+++ cogs/benefits/likedby.py
@@ -0,0 +1,26 @@
+"""
+SEE WHO LIKED YOU
+"""
+import discord
+from discord.ext import commands
+from config import PREFIX, EMBED_COLOR
+from discord.ui import Button, View
+from checks.voted import is_voter
+
+
+class LikedBy(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @is_voter()
+    @commands.command(name="likedby")
+    async def likedby(self, ctx):
+        data = await self.bot.db.find_one({"_id": str(ctx.author.id)})
+        data = data["liked_by"]
+        users = [await self.bot.fetch_user(int(user)) for user in data]
+        users = [f"{user.name}#{user.discriminator}" for user in users]
+        
+        return await ctx.author.send(embed=discord.Embed(title="You were liked by these users:", description=", ".join(users), color=EMBED_COLOR))
+
+async def setup(bot):
+    await bot.add_cog(LikedBy(bot))
