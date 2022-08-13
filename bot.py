c1e43b0e47989a40f41cec4a61860ffd0dd85e79
jannes.t13@gmail.com <jannes.t13@gmail.com>
2022-08-13 15:31:38 +0200

    v1.5.2

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ class Bot(commands.AutoShardedBot):
             activity=discord.Activity(type=discord.ActivityType.watching, name=f"{PREFIX}help")
         )
         self.launch = __import__("datetime").datetime.utcnow()
-        self.version = "v1.5.1"
+        self.version = "v1.5.2"
         self.creator = "Bambus#8446"
         self.ownerid = OWNERID
 
