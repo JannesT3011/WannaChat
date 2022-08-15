e65c4d8269dcec94f5e3be42f0f00310fee49d77
jannes.t13@gmail.com <jannes.t13@gmail.com>
2022-08-15 13:11:36 +0200

    Update sync command

--- cogs/owner/sync.py
+++ cogs/owner/sync.py
@@ -1,15 +1,20 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
+
+def is_owner(interaction:discord.Interaction):
+    return interaction.user.id == 311450463553847299
 
 class Sync(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    @commands.command(name="sync")
-    @commands.is_owner()
-    async def sync(self, ctx):
-        await ctx.bot.tree.sync()
-        return await ctx.reply("Synced!")
+
+    @app_commands.command()
+    @app_commands.check(is_owner)
+    async def sync(self, interaction:discord.Interaction):
+        self.bot.tree.copy_global_to(guild=discord.Object(id=364335676549890048))
+        await self.bot.tree.sync(guild=discord.Object(id=364335676549890048))
+        await interaction.response.send_message("Done!")
     
 async def setup(bot):
     await bot.add_cog(Sync(bot))
