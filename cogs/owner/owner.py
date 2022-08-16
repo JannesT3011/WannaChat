bcbbe84ef1bdd03b0443113a6d9708d0748aedb0
jannes.t13@gmail.com <jannes.t13@gmail.com>
2022-08-16 16:34:08 +0200

    Convert all owner commands to slash commands

--- cogs/owner/owner.py
+++ cogs/owner/owner.py
@@ -1,27 +1,35 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
+from checks.owner_check import is_owner
+from typing import Literal
+
+TEST_GUILD = discord.Object(364335676549890048)
 
 class Owner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.is_owner()
-    @commands.command(name='users', aliases=["user"])
-    async def users(self, ctx):
+    @app_commands.command(name='users', description="Get the total users")
+    @app_commands.guilds(TEST_GUILD)
+    @app_commands.check(is_owner)
+    async def users(self, interaction: discord.Interaction):
         """GET THE AMOUNT OF BOT USERS"""
         data = await self.bot.queuedb.find_one({"_id": "queue"})
 
-        return await ctx.author.send(f"We are currently {len(data['queue'])} users")
+        return await interaction.response.send_message(f"We are currently {len(data['queue'])} users")
     
-    @commands.is_owner()
-    @commands.command(name="servers", aliases=["guilds"])
-    async def guilds(self, ctx):
+    @app_commands.command(name="servers", description="Get the server count")
+    @app_commands.guilds(TEST_GUILD)
+    @app_commands.check(is_owner)
+    async def guilds(self, interaction: discord.Interaction):
         """GET THE AMOUNT OF BOT GUILDS"""
-        return await ctx.author.send(f"Im currenty on {len(self.bot.guilds)} server(s)")
+        return await interaction.response.send_message(f"Im currenty on {len(self.bot.guilds)} server(s)")
     
-    @commands.is_owner()
-    @commands.command(name="status")
-    async def status(self, ctx, type:str, *, game:str):
+    @app_commands.command(name="status", description="Change the status of the Bot")
+    @app_commands.guilds(TEST_GUILD)
+    @app_commands.check(is_owner)
+    async def status(self, interaction: discord.Interaction, type:Literal["game", "watching", "listening"], game:str):
         """SET THE BOT STATUS"""
         if type == "game":
             await self.bot.change_presence(activity=discord.Game(name=game))
@@ -30,28 +38,30 @@ class Owner(commands.Cog):
         elif type == "listening":
             await self.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=game))
         else:
-            return await ctx.author.send("No valid type -> `game, watching, listening`")
+            return await interaction.response.send_message("No valid type -> `game, watching, listening`")
 
-        return await ctx.author.send(f"Status set to {game}")
+        return await interaction.response.send_message(f"Status set to {game}")
     
-    @commands.is_owner()
-    @commands.command(name="stats")
-    async def stats(self, ctx):
+    @app_commands.command(name="stats", description="Stats about the bot")
+    @app_commands.guilds(TEST_GUILD)
+    @app_commands.check(is_owner)
+    async def stats(self, interaction: discord.Interaction):
         """SHOW USERS AND SERVERS IN ONE MESSAGE"""
         data = await self.bot.queuedb.find_one({"_id": "queue"})
         gc_data = await self.bot.gcserversdb.find_one({"_id": "servers"})
 
-        return await ctx.author.send(embed=discord.Embed(title="Stats", description=f"**Servers:** {len(self.bot.guilds)}\n**Users:** {len(data['queue'])}\n**GC Channels:**{len(gc_data['channels'])}", timestamp=ctx.message.created_at))
+        return await interaction.response.send_message(embed=discord.Embed(title="Stats", description=f"**Servers:** {len(self.bot.guilds)}\n**Users:** {len(data['queue'])}\n**GC Channels:**{len(gc_data['channels'])}", timestamp=interaction.created_at))
 
-    @commands.is_owner()
-    @commands.command(name="gcban")
-    async def gcban(self, ctx, user:int):
+    @app_commands.command(name="gcban", description="Ban user from GlobalChat")
+    @app_commands.guilds(TEST_GUILD)
+    @app_commands.check(is_owner)
+    async def gcban(self, interaction: discord.Interaction, user:int):
         await self.bot.gcserversdb.update_many({"_id": "servers"}, {"$push": {"blacklist": user}})
 
         try:
             user = await self.bot.fetch_user(user)
         except:
-            return await ctx.send(embed=discord.Embed(title=f"{user} banned!"))
+            return await interaction.response.send_message(embed=discord.Embed(title=f"{user} banned!"))
 
         globalchat_data = await self.bot.gcserversdb.find_one({"_id": "servers"})
 
@@ -64,7 +74,7 @@ class Owner(commands.Cog):
             except:
                 continue
 
-        return await ctx.author.send(embed=discord.Embed(title=f"{user.name} banned!"))
+        return await interaction.response.send_message(embed=discord.Embed(title=f"{user.name} banned!"))
 
 async def setup(bot):
     await bot.add_cog(Owner(bot))
--- cogs/owner/sync.py
+++ cogs/owner/sync.py
@@ -1,20 +1,21 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+from checks.owner_check import is_owner
 
-def is_owner(interaction:discord.Interaction):
-    return interaction.user.id == 311450463553847299
+TEST_GUILD = discord.Object(364335676549890048)
 
 class Sync(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command()
+    @app_commands.guilds(TEST_GUILD)
     @app_commands.check(is_owner)
     async def sync(self, interaction:discord.Interaction):
-        self.bot.tree.copy_global_to(guild=discord.Object(id=364335676549890048))
-        await self.bot.tree.sync(guild=discord.Object(id=364335676549890048))
-        await interaction.response.send_message("Done!")
+        self.bot.tree.copy_global_to(guild=TEST_GUILD)
+        await self.bot.tree.sync(guild=TEST_GUILD)
+        await interaction.response.send_message("Commands synced!")
     
 async def setup(bot):
     await bot.add_cog(Sync(bot))
