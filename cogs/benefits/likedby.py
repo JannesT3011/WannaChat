ba27c82288a4b8681a12388826c5a079712bd277
jannes.t13@gmail.com <jannes.t13@gmail.com>
2022-09-19 22:04:45 +0200

    Remove some useless code and fix some bugs

--- cogs/benefits/likedby.py
+++ cogs/benefits/likedby.py
@@ -6,8 +6,7 @@ from discord import app_commands
 from discord.ext import commands
 from config import PREFIX, EMBED_COLOR
 from discord.ui import Button, View
-from checks.voted import is_voter, voted
-from checks.registered import is_registered, registered
+from checks import is_voter, is_registered
 
 
 class LikedBy(commands.Cog):
@@ -18,15 +17,14 @@ class LikedBy(commands.Cog):
     @is_registered()
     @is_voter()
     async def likedby(self, interaction: discord.Interaction):
-        if await registered(self.bot, interaction.user) and await voted(self.bot, interaction.user):
-            data = await self.bot.db.find_one({"_id": str(interaction.user.id)})
-            data = data["liked_by"]
-            users = [await self.bot.fetch_user(int(user)) for user in data]
-            users = [f"`{user.name}#{user.discriminator}`" for user in users]
-            
-            await interaction.response.defer(ephemeral=True, thinking=True)
-            if data:
-                return await interaction.followup.send(embed=discord.Embed(title="You were liked by these users:", description=", ".join(users), color=EMBED_COLOR).set_footer(text="Add one of the users who liked you and start chatting 🙊", icon_url=self.bot.user.display_avatar.url), ephemeral=True)
+        data = await self.bot.db.find_one({"_id": str(interaction.user.id)})
+        data = data["liked_by"]
+        users = [await self.bot.fetch_user(int(user)) for user in data]
+        users = [f"`{user.name}#{user.discriminator}`" for user in users]
+        
+        await interaction.response.defer(ephemeral=True, thinking=True)
+        if data:
+            return await interaction.followup.send(embed=discord.Embed(title="You were liked by these users:", description=", ".join(users), color=EMBED_COLOR).set_footer(text="Add one of the users who liked you and start chatting 🙊", icon_url=self.bot.user.display_avatar.url), ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(LikedBy(bot))
--- cogs/benefits/reset.py
+++ cogs/benefits/reset.py
@@ -8,8 +8,8 @@ from discord.ext import commands
 from discord import app_commands
 from config import PREFIX, EMBED_COLOR
 from discord.ui import Button, View
-from checks.voted import is_voter, voted
-from checks.registered import is_registered, registered
+from checks.voted import is_voter
+from checks import is_voter, is_registered
 
 class Reset(commands.Cog):
     def __init__(self, bot):
@@ -23,25 +23,23 @@ class Reset(commands.Cog):
     @app_commands.checks.cooldown(1, 180.0, key=lambda i: (i.guild_id, i.user.id))
     async def reset(self, interaction: discord.Interaction):
         """RESET YOUR LIKES AND DISLIKES"""
-        if await registered(self.bot, interaction.user) and await voted(self.bot, interaction.user):
-            async def confirm_button_interaction(interaction):
-                await self.bot.db.update_many({"_id": str(interaction.user.id)}, {"$set": {"disliked_users": []}})
-                await self.bot.db.update_many({"_id": str(interaction.user.id)}, {"$set": {"liked_users": []}})
+        async def confirm_button_interaction(interaction):
+            await self.bot.db.update_many({"_id": str(interaction.user.id)}, {"$set": {"disliked_users": []}})
+            await self.bot.db.update_many({"_id": str(interaction.user.id)}, {"$set": {"liked_users": []}})
+            return await interaction.response.send_message(embed=discord.Embed(title="Successfully reseted your liked and disliked users!", color=EMBED_COLOR), ephemeral=True)
+        
+        async def cancel_button_interaction(interaction):
+            await interaction.response.edit_message(embed=discord.Embed(title="Click `dismiss message` to end"), view=None)
+        
+        confirm_button = Button(label="Yes", style=discord.ButtonStyle.red, emoji="✅")
+        cancel_button = Button(label="Cancel", style=discord.ButtonStyle.green, emoji="❌")
+        confirm_button.callback = confirm_button_interaction
+        cancel_button.callback = cancel_button_interaction
+        view = View()
+        view.add_item(confirm_button)
+        view.add_item(cancel_button)
 
-                return await interaction.response.send_message(embed=discord.Embed(title="Successfully reseted your liked and disliked users!", color=EMBED_COLOR), ephemeral=True)
-
-            async def cancel_button_interaction(interaction):
-                await interaction.response.edit_message(embed=discord.Embed(title="Click `dismiss message` to end"), view=None)
-
-            confirm_button = Button(label="Yes", style=discord.ButtonStyle.red, emoji="✅")
-            cancel_button = Button(label="Cancel", style=discord.ButtonStyle.green, emoji="❌")
-            confirm_button.callback = confirm_button_interaction
-            cancel_button.callback = cancel_button_interaction
-            view = View()
-            view.add_item(confirm_button)
-            view.add_item(cancel_button)
-
-            await interaction.response.send_message(embed=discord.Embed(title="Are you sure you want to reset your liked and disliked users?", color=EMBED_COLOR), view=view, ephemeral=True)
+        await interaction.response.send_message(embed=discord.Embed(title="Are you sure you want to reset your liked and disliked users?", color=EMBED_COLOR), view=view, ephemeral=True)
 
     @reset_group.command(name="likes", description="Reset your likes")
     @is_registered()
@@ -49,47 +47,45 @@ class Reset(commands.Cog):
     @app_commands.checks.cooldown(1, 180.0, key=lambda i: (i.guild_id, i.user.id))
     async def reset_likes(self, interaction: discord.Interaction):
         """RESET YOUR LIKES"""
-        if await registered(self.bot, interaction.user) and await voted(self.bot, interaction.user):
-            async def confirm_button_interaction(interaction):
-                await self.bot.db.update_many({"_id": str(interaction.user.id)}, {"$set": {"liked_users": []}})
-                return await interaction.response.send_message(embed=discord.Embed(title="Successfully reseted your liked users!", color=EMBED_COLOR), ephemeral=True)
-            
-            async def cancel_button_interaction(interaction):
-                await interaction.response.edit_message(embed=discord.Embed(title="Click `dismiss message` to end"), view=None)
+        async def confirm_button_interaction(interaction):
+            await self.bot.db.update_many({"_id": str(interaction.user.id)}, {"$set": {"liked_users": []}})
+            return await interaction.response.send_message(embed=discord.Embed(title="Successfully reseted your liked users!", color=EMBED_COLOR), ephemeral=True)
+        
+        async def cancel_button_interaction(interaction):
+            await interaction.response.edit_message(embed=discord.Embed(title="Click `dismiss message` to end"), view=None)
+        
+        confirm_button = Button(label="Yes", style=discord.ButtonStyle.red, emoji="✅")
+        cancel_button = Button(label="Cancel", style=discord.ButtonStyle.green, emoji="❌")
+        confirm_button.callback = confirm_button_interaction
+        cancel_button.callback = cancel_button_interaction
+        view = View()
+        view.add_item(confirm_button)
+        view.add_item(cancel_button)
+        
+        await interaction.response.send_message(embed=discord.Embed(title="Are you sure you want to reset your liked users?", color=EMBED_COLOR), view=view, ephemeral=True)
 
-            confirm_button = Button(label="Yes", style=discord.ButtonStyle.red, emoji="✅")
-            cancel_button = Button(label="Cancel", style=discord.ButtonStyle.green, emoji="❌")
-            confirm_button.callback = confirm_button_interaction
-            cancel_button.callback = cancel_button_interaction
-            view = View()
-            view.add_item(confirm_button)
-            view.add_item(cancel_button)
-
-            await interaction.response.send_message(embed=discord.Embed(title="Are you sure you want to reset your liked users?", color=EMBED_COLOR), view=view, ephemeral=True)
-
-    #@is_registered()
-    #@is_voter()
+    @is_registered()
+    @is_voter()
     @commands.cooldown(1, 180.0, commands.BucketType.user)
     @reset_group.command(name="dislikes", description="Reset your dislikes")
     async def reset_dislikes(self, interaction: discord.Interaction):
         """RESET YOUR DISLIKES"""
-        if await registered(self.bot, interaction.user) and await voted(self.bot, interaction.user):
-            async def confirm_button_interaction(interaction):
-                await self.bot.db.update_many({"_id": str(interaction.user.id)}, {"$set": {"disliked_users": []}})
-                return await interaction.response.send_message(embed=discord.Embed(title="Successfully reseted your disliked users!", color=EMBED_COLOR), ephemeral=True)
-            
-            async def cancel_button_interaction(interaction):
-                await interaction.response.edit_message(embed=discord.Embed(title="Click `dismiss message` to end"), view=None, ephemeral=True)
+        async def confirm_button_interaction(interaction):
+            await self.bot.db.update_many({"_id": str(interaction.user.id)}, {"$set": {"disliked_users": []}})
+            return await interaction.response.send_message(embed=discord.Embed(title="Successfully reseted your disliked users!", color=EMBED_COLOR), ephemeral=True)
+        
+        async def cancel_button_interaction(interaction):
+            await interaction.response.edit_message(embed=discord.Embed(title="Click `dismiss message` to end"), view=None)
+        
+        confirm_button = Button(label="Yes", style=discord.ButtonStyle.red, emoji="✅")
+        cancel_button = Button(label="Cancel", style=discord.ButtonStyle.green, emoji="❌")
+        confirm_button.callback = confirm_button_interaction
+        cancel_button.callback = cancel_button_interaction
+        view = View()
+        view.add_item(confirm_button)
+        view.add_item(cancel_button)
 
-            confirm_button = Button(label="Yes", style=discord.ButtonStyle.red, emoji="✅")
-            cancel_button = Button(label="Cancel", style=discord.ButtonStyle.green, emoji="❌")
-            confirm_button.callback = confirm_button_interaction
-            cancel_button.callback = cancel_button_interaction
-            view = View()
-            view.add_item(confirm_button)
-            view.add_item(cancel_button)
-
-            await interaction.response.send_message(embed=discord.Embed(title="Are you sure you want to reset your disliked users?", color=EMBED_COLOR), view=view, ephemeral=True)
+        await interaction.response.send_message(embed=discord.Embed(title="Are you sure you want to reset your disliked users?", color=EMBED_COLOR), view=view, ephemeral=True)
 
 
 async def setup(bot):
