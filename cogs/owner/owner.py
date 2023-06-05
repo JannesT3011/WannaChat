b49393f1c3388c7cc8b196494f53d0ca1b4d0e9e
Jannes <36892200+Bmbus@users.noreply.github.com>
2023-06-05 11:40:04 +0200

    Add 'remove deleted users function'

--- cogs/owner/owner.py
+++ cogs/owner/owner.py
@@ -1,14 +1,16 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord import app_commands
 from checks import is_owner
 from typing import Literal
+from database.database import Database
 
 TEST_GUILD = discord.Object(364335676549890048)
 
 class Owner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.remove_deleted_users_task.start()
 
     @app_commands.command(name='users', description="Get the total users")
     @app_commands.guilds(TEST_GUILD)
@@ -53,6 +55,16 @@ class Owner(commands.Cog):
         if data:
             return await interaction.followup.send(embed=discord.Embed(title="Stats", description=f"**Servers:** {len(self.bot.guilds)}\n**Users:** {len(data['queue'])}\n", timestamp=interaction.created_at), ephemeral=True)
     
+    @app_commands.command(name="delete_users", description="Delete all non existing users")
+    @app_commands.guilds(TEST_GUILD)
+    @app_commands.check(is_owner)
+    async def delete_users(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Remove all deleted users!", ephemeral=True)
+        del_users = await self.remove_deleted_users()
+
+        return await interaction.user.send(f"Done! Deleted {len(del_users)} users!", ephemeral=True)
+
+
     @app_commands.command(name="gcban", description="Ban user from GlobalChat")
     @app_commands.guilds(TEST_GUILD)
     @app_commands.check(is_owner)
@@ -76,6 +88,24 @@ class Owner(commands.Cog):
                 continue
 
         return await interaction.response.send_message(embed=discord.Embed(title=f"{user.name} banned!"), ephemeral=True)
+
+    @tasks.loop(hours=24)
+    async def remove_deleted_users_task(self):
+        del_users = await self.remove_deleted_users()
+        print(f"Remove {del_users} users!")
+
+    async def remove_deleted_users(self) -> list:
+        deleted = []
+        uids = await self.bot.db.distinct("_id")
+        for id in uids:
+            user = await self.bot.fetch_user(int(id))
+            if user.name.startswith("Deleted User"):
+                await Database().delete_db(str(user.id))
+                await self.bot.queuedb.update_many({"_id": "queue"}, {"$pull": {"queue": str(user.id)}})
+                print(f"Remove {user.name}, {user.id}")
+                deleted.append(id)
+        return deleted
+
 
 async def setup(bot):
     await bot.add_cog(Owner(bot))
